af89322dad5fb35e9de4e4819a0b4c26eb43c057
Karolpineda <karolpineda@hotmail.com>
2025-02-05 02:58:35 -0500

    feat: :truck: Add file of controller

---
+++ app/controllers/user.py
@@ -0,0 +1,25 @@
+# app/controllers/user.py
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import schemas, database
+from app.services.user import update_user, get_user_by_id
+
+
+router = APIRouter()
+# Ruta Health para verificar si el microservicio está activo
+@router.get("/health")
+def health_check():
+    return {"status": "Microservice Users is up and running"}
+
+
+# Ruta para actualizar un usuario
+@router.put("/users/{user_id}", response_model=schemas.UserOut)
+def update_user_route(user_id: str, user: schemas.UserUpdate, db: Session = Depends(database.get_db)):
+    # Verificar si el usuario existe
+    db_user = get_user_by_id(db, user_id=user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    # Actualizar los campos
+    return update_user(db=db, user_id=user_id, user=user)
+
