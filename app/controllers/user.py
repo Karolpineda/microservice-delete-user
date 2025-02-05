2f8713799ed436cf55a691a53b366c74e04f7159
Karolpineda <karolpineda@hotmail.com>
2025-02-05 03:15:38 -0500

    feat: :sparkles: Update controller for delete

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,8 +1,7 @@
-# app/controllers/user.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import schemas, database
-from app.services.user import update_user, get_user_by_id
+from app.services.user import delete_user
+from app.database import get_db
 
 
 router = APIRouter()
@@ -13,13 +12,11 @@ def health_check():
 
 
 # Ruta para actualizar un usuario
-@router.put("/users/{user_id}", response_model=schemas.UserOut)
-def update_user_route(user_id: str, user: schemas.UserUpdate, db: Session = Depends(database.get_db)):
-    # Verificar si el usuario existe
-    db_user = get_user_by_id(db, user_id=user_id)
-    if not db_user:
+@router.delete("/users/{user_id}")
+def delete_user_endpoint(user_id: str, db: Session = Depends(get_db)):
+    success = delete_user(db, user_id)
+    
+    if not success:
         raise HTTPException(status_code=404, detail="User not found")
     
-    # Actualizar los campos
-    return update_user(db=db, user_id=user_id, user=user)
-
+    return {"message": "User deleted successfully"}
